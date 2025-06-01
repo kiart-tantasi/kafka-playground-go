c06e827862a5ea17c19a927baba25471e59162ef
kiart-tantasi <kiarttantasi@gmail.com>
2025-06-01 13:16:13 +0700

    add counter logs

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -1,3 +1,15 @@
+/*
+Experiment1:
+
+	what happens
+	if consumer continuously reads message from topic(s)
+	and there is another goroutine to make that consumer subscribe different topic(s)
+
+Result:
+	after resubscribng to new topic(s),
+	consumer finishes the current message of the first topic(s) and change to read message from the new topic(s0)
+*/
+
 package main
 
 import (
@@ -31,7 +43,7 @@ func main() {
 	// Topic switcher
 	go func() {
 		for {
-			time.Sleep(10_000 * time.Millisecond)
+			time.Sleep(5_000 * time.Millisecond)
 
 			// switch
 			if currentTopics[0] == "test1" {
@@ -45,14 +57,18 @@ func main() {
 		}
 	}()
 
+	counterTest1 := 0
+	counterTest2 := 0
+	counterOther := 0
+
 	for {
 		// Info
-		if subcribedTopics, err := consumer.Subscription(); err != nil {
-			log.Printf("Error while getting subscription info: %s", err)
-			continue
-		} else {
-			log.Printf("Reading a message from topics %s", subcribedTopics)
-		}
+		// if subcribedTopics, err := consumer.Subscription(); err != nil {
+		// 	log.Printf("Error while getting subscription info: %s", err)
+		// 	continue
+		// } else {
+		// 	log.Printf("Reading a message from topics %s", subcribedTopics)
+		// }
 
 		// Read
 		msg, err := consumer.ReadMessage(-1)
@@ -60,7 +76,22 @@ func main() {
 			log.Printf("Error while reading message: %s", err)
 			continue
 		}
-		log.Printf("Message: %s", string(msg.Value))
+
+		// Process message
+		message := string(msg.Value)
+		// Fake process time
+		time.Sleep(5 * time.Millisecond)
+
+		// Count
+		// log.Printf("Message: %s", message)
+		if message == "test1" {
+			counterTest1++
+		} else if message == "test2" {
+			counterTest2++
+		} else {
+			counterOther++
+		}
+		log.Printf("counterTest1: [%d], counterTest2: [%d], counterOther: [%d]", counterTest1, counterTest2, counterOther)
 	}
 
 }
